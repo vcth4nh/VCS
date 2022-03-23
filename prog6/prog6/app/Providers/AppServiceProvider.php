ed8319ae59a71e3017cdbc39e5b21f00b3140ac7
vcth4nh <tvu25337@gmail.com>
2022-03-23 22:27:19 +0700

    done dashboard and msg

--- prog6/prog6/app/Providers/AppServiceProvider.php
+++ prog6/prog6/app/Providers/AppServiceProvider.php
@@ -27,14 +27,20 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Validator::extend('not_exist_with_another_uid', function ($attribute, $value, $uid) {
-            return !User::where('uid', '!=', $uid)->where('username', $value)->exists();
+            return !User::query()
+                ->where('uid', '!=', $uid)
+                ->where('username', $value)
+                ->exists();
         }, __('validation.not_exist_with_another_uid'));
 
         Validator::extend('phone_number', function ($attribute, $value) {
             return preg_match('/^\d{5,15}$/', $value);
         }, __('validation.phone_number'));
         Validator::extend('is_student', function ($attribute, $value) {
-            return User::where('uid', $value)->where('role', STUDENT)->exists();
-        },__('validation.is_student'));
+            return User::query()
+                ->where('uid', $value)
+                ->where('role', STUDENT)
+                ->exists();
+        }, __('validation.is_student'));
     }
 }
