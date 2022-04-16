60ee352d19149a904789c143881c265033162e29
vcth4nh <tvu25337@gmail.com>
2022-04-16 22:39:26 +0700

    Remove ignored files

---
+++ prog3/sshtrojan2/openssh-portable/regress/unittests/misc/tests.c
@@ -0,0 +1,37 @@
+/* 	$OpenBSD: tests.c,v 1.8 2021/12/14 21:25:27 deraadt Exp $ */
+/*
+ * Regress test for misc helper functions.
+ *
+ * Placed in the public domain.
+ */
+
+#include "includes.h"
+
+#include <sys/types.h>
+#include <stdio.h>
+#ifdef HAVE_STDINT_H
+#include <stdint.h>
+#endif
+#include <stdlib.h>
+#include <string.h>
+
+#include "../test_helper/test_helper.h"
+
+#include "log.h"
+#include "misc.h"
+
+void test_parse(void);
+void test_convtime(void);
+void test_expand(void);
+void test_argv(void);
+void test_strdelim(void);
+
+void
+tests(void)
+{
+	test_parse();
+	test_convtime();
+	test_expand();
+	test_argv();
+	test_strdelim();
+}
