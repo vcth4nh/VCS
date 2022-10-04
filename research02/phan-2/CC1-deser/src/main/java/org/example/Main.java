353ede52f0431c16bf44bed8ce862a7e6850ca09
vcth4nh <tvu25337@gmail.com>
2022-10-05 01:03:38 +0700

    new research02

---
+++ research02/phan-2/CC1-deser/src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.io.*;
+
+public class Main {
+    private static String getResourcePath(String fileName) {
+        return "src/main/resources/" + fileName;
+    }
+
+    private static void deserializeFromFile(String fileName) {
+        try {
+            FileInputStream fis = new FileInputStream(getResourcePath(fileName));
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            ois.readObject();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        deserializeFromFile("payload_calc.bin");
+    }
+}
+
